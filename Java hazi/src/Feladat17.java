a33a7ec1294d98fa899d2918275077d5c5bc85cf
berkyd <berky.daniel@petrik.hu>
2022-09-26 19:29:12 +0200

    17feladat

---
+++ Java hazi/src/Feladat17.java
@@ -0,0 +1,10 @@
+public class Feladat17 {
+    public static void main(String[] args) {
+        double[] t1 = new double[5];
+        double[] t2 = new double[5];
+        double[] t3 = new double[5];
+        for (int i = 0; i<t1.length; i++){
+            t3[i] = (t1[i] + t2[i]);
+        }
+    }
+}
