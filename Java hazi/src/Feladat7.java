ea6ee56727d97d07be4924df7170b904b68a3afa
berkyd <berky.daniel@petrik.hu>
2022-09-26 19:04:33 +0200

    7feladat

---
+++ Java hazi/src/Feladat7.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Feladat7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Kérem adja meg hanyadik hónap van jelenleg");
+        double jelenlegiHonap = sc.nextDouble();
+    }
+}
